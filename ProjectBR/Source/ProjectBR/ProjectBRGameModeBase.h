6e8e630bfb08ec113b43019c5f9d371dfa585eb3
homveloper <bum4496@naver.com>
2022-03-24 21:31:46 +0900

    초기화 : 프로젝트

---
+++ ProjectBR/Source/ProjectBR/ProjectBRGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectBRGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTBR_API AProjectBRGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
